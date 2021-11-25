52fc4d4cfc0362bea27b6676c8ce7528e02ff2a1
stivgo <stiveng0630@gmail.com>
2021-11-25 16:57:39 -0500

    Add var colors

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,24 @@
 </template>
 
 <style>
+:root {
+  --first-color: #353535;
+  --second-color:#5E5E5E;
+  --third-color: #F22539;
+  --fourth-color: #C00E20;
+  --fifth-color: #BFBFBF;
+  --sixth-color: #ECA539;
+}
+
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Lato', sans-serif;
+  border: none;
+}
+
+button{
+  cursor: pointer;
 }
 </style>
