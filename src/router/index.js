8d7ad46096b7392382b8df47707c2d83323f0a5f
stivgo <stiveng0630@gmail.com>
2021-11-25 15:07:33 -0500

    Add views on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Welcome from '../views/Welcome.vue';
+import Results from '../views/Results.vue';
+import Favorites from '../views/Favorites.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'welcome',
+    component: Welcome,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/results',
+    name: 'results',
+    component: Results,
   },
+  {
+    path: '/favorites',
+    name: 'favorites',
+    component: Favorites,
+  }
+
 ];
 
 const router = new VueRouter({
