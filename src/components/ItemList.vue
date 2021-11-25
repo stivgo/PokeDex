6209cc498e6cb60fa2f990ab037146791811541b
stivgo <stiveng0630@gmail.com>
2021-11-25 15:49:24 -0500

    Create ItemList Component

---
+++ src/components/ItemList.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="item-list">
+    <p>Pokemon {{ item }}</p>
+    <StarFavorite />
+  </article>
+</template>
+
+<script>
+import StarFavorite from './StarFavorite.vue';
+
+export default {
+  name: 'ItemList',
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    StarFavorite,
+  },
+  data() {
+    return {
+      selectedItem: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
