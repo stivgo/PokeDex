85b8c67ac47979c9ebf5ea74b6fe34dd06fcf253
stivgo <stiveng0630@gmail.com>
2021-11-25 15:49:12 -0500

    Create InputSearch Component

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="input-search">
+    <i class="fas fa-search"></i>
+    <input type="text" v-model="search" @keyup.enter="searchPokemon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputSearch',
+  data() {
+    return {
+      search: '',
+    };
+  },
+  methods: {
+    searchPokemon() {
+      // this.$emit('search', this.search)
+    },
+  },
+};
+</script>
+
+<style>
+</style>
