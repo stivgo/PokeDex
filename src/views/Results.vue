9ec64bc4e8580871fd8974b187095a52f43551ed
stivgo <stiveng0630@gmail.com>
2021-11-25 22:35:00 -0500

    Update Views

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,12 +1,140 @@
 <template>
-  <div class="results"></div>
+  <div class="results">
+    <Loader v-if="loading" />
+    <div class="info" v-else>
+      <header>
+        <InputSearch />
+      </header>
+      <div v-if="isSearch">
+        <section v-if="!pokemonSearch.name" class="no-result">
+          <h2>Uh-oh!</h2>
+          <p>You look lost on your journey!</p>
+          <Button :text="'Go back home'" @click="getPokemons" />
+        </section>
+        <section v-else>
+          <ItemList :name="pokemonSearch.name" />
+        </section>
+      </div>
+      <div v-else>
+        <section>
+          <ItemList
+            v-for="(item, index) of pokemons"
+            :key="index"
+            :name="item.name"
+          />
+        </section>
+      </div>
+      <footer v-if=" !isSearch || pokemonSearch.name">
+        <Button :text="'All'" :all="true" />
+        <Button :text="'Favorites'" :favorite="true" :disabled="true" />
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
+import Loader from '../components/Loader.vue';
+import InputSearch from '../components/InputSearch.vue';
+import ItemList from '../components/ItemList.vue';
+import Button from '../components/Button.vue';
+
 export default {
   name: 'Results',
+  components: {
+    Loader,
+    InputSearch,
+    ItemList,
+    Button,
+  },
+  created() {
+    this.getPokemons();
+  },
+  methods: {
+    getPokemons() {
+      this.$store.dispatch('pokemon/getPokemons');
+    },
+  },
+  computed: {
+    loading() {
+      return this.$store.state.pokemon.loading;
+    },
+    pokemons() {
+      return this.$store.state.pokemon.pokemons;
+    },
+    pokemonSearch() {
+      return this.$store.state.pokemon.pokemonSearch;
+    },
+    isSearch() {
+      return this.$store.state.pokemon.isSearch;
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.results {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  background: #f5f5f5;
+}
+
+header input {
+  width: 95%;
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+.no-result {
+  height: 30vh;
+  justify-content: space-around;
+}
+
+.no-result h2 {
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 43px;
+  color: #353535;
+}
+
+.no-result p {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 150%;
+  color: #5e5e5e;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0px -5px 4px rgba(0, 0, 0, 0.05);
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+footer button {
+  width: 23%;
+  margin: 1%;
+}
 </style>
