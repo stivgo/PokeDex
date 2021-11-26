2709398f2c98c3e2aa1074d6dbea7e6427103fbc
stivgo <stiveng0630@gmail.com>
2021-11-25 22:34:33 -0500

    Update StarFavorite Component

--- src/components/StarFavorite.vue
+++ src/components/StarFavorite.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="star-favorite">
-    <i class="fas fa-star"></i>
+    <i :class="('fas fa-star')+ (isFavorite ? ' active ': ' disabled ')"></i>
   </div>
 </template>
 
 <script>
 export default {
   name: 'StarFavorite',
+  props: {
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.star-favorite {
+  background: #F5F5F5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 50px;
+}
+
+.star-favorite i{
+  font-size: 1.5rem;
+}
+
+.active{
+  color :#ECA539;
+}
+
+.disabled{
+  color: #BFBFBF;
+}
 </style>
